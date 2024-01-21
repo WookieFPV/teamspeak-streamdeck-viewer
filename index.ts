b5f7d5f8e38dc406067290a7c2ec971bb2960cd1
Lukas Müller <WookieFPV@users.noreply.github.com>
2024-01-21 14:18:54 +0100

    add super quick and dirty code to show ts3 clients

--- index.ts
+++ index.ts
@@ -1,19 +1,22 @@
 import {openStreamDeck, listStreamDecks, StreamDeck,} from '@elgato-stream-deck/node'
+import {TeamSpeak, TeamSpeakClient} from "ts3-nodejs-library";
 import path from 'path'
 import sharp from 'sharp'
+import {tsConnect} from "./ts-base";
+import {wait} from "./helper";
 
 const colors = {
   black: "black",
   blue: "blue",
   light_blue: "light_blue",
   red: "red",
-  none: "none"
+  orange: "orange"
 } as const
 
 type Colors = keyof typeof colors
 
-const finalBufferOffline = ()=> sharp(path.resolve(__dirname, `assets/black.png`))
-  .composite( [
+const finalBufferOffline = () => sharp(path.resolve(__dirname, `assets/black.png`))
+  .composite([
     {
       input: Buffer.from(
         `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80" version="1.1">/
@@ -39,8 +42,8 @@ const finalBufferOffline = ()=> sharp(path.resolve(__dirname, `assets/black.png`
 
 const paint = async (streamDeck: StreamDeck, index: number, name: string, color: Colors) => {
   try {
-    const finalBuffer = color === "none"? await finalBufferOffline() : await sharp(path.resolve(__dirname, `assets/${color}.png`))
-      .composite(  [
+    const finalBuffer = await sharp(path.resolve(__dirname, `assets/${color}.png`))
+      .composite([
         {
           input: Buffer.from(
             `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${streamDeck.ICON_SIZE} ${
@@ -98,16 +101,51 @@ const fun = async () => {
   return streamDeck
 }
 
+export const clientStateToColor = (client: TeamSpeakClient): Colors => {
+ if (client.flagTalking) {
+    return "light_blue"
+  } else if (client.inputMuted && !client.outputMuted) {
+    return "orange"
+  } else if (client.outputMuted) {
+    return "red"
+  } else {
+    return "blue"
+  }
+}
+
+const drawTS = async (streamDeck: StreamDeck, ts: TeamSpeak) => {
+  const clientsRaw = await ts.clientList();
+  const clients = clientsRaw.filter(c => c.type == 0)
+
+
+  const clientList = clients.map(f => f.nickname)
+  console.log("clients:" + JSON.stringify(clientList))
+
+
+  for (let i = 0; i < clients.length; i++) {
+    const client = clients[i]
+    await paint(streamDeck, i, client.nickname, clientStateToColor(client))
+  }
+  for (let i = clients.length; i < 6; i++) {
+    await streamDeck.clearKey(i)
+  }
+}
 
 const run = async () => {
   const streamDeck = await fun()
 
-  await paint(streamDeck, 0, "wookie", "red")
-  await paint(streamDeck, 1, "N1m4", "blue")
-  await paint(streamDeck, 2, "jonas", "light_blue")
-  await paint(streamDeck, 3, "felix", "none")
-  await paint(streamDeck, 4, "luki", "red")
-  await paint(streamDeck, 5, "Peter", "red")
+  for (let i = 0; i < 6; i++) {
+    await streamDeck.clearKey(i)
+  }
+
+
+  const ts = await tsConnect()
+
+  while (true) {
+    await drawTS(streamDeck,ts)
+    await wait(500);
+  }
+
 }
 
 
