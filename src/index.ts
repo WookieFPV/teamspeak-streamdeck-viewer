37383374dbb104307821d99e6a726b208e7027b8
Lukas Müller <WookieFPV@users.noreply.github.com>
2024-02-03 00:15:22 +0100

    reduce polling rate and instead check clients on event

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { TeamSpeakClient } from "ts3-nodejs-library";
 import { tsConnect } from "./teamspeak/ts-base";
 import { wait, waitForNetwork } from "./helper";
-import { streamDeckConnect } from "./streamdeck";
+import { drawClock, streamDeckConnect } from "./streamdeck";
 import { TsDrawClients } from "./teamspeak/tsPrintClients";
 import { envVars } from "./envVars";
 
@@ -14,15 +14,15 @@ const run = async () => {
   console.log("started")
   const streamDeck = await streamDeckConnect()
   await streamDeck.clearPanel()
- if(envVars.DEBUG_UI) await streamDeck.fillKeyColor(0, 0, 0, 100)
+  if (envVars.DEBUG_UI) await streamDeck.fillKeyColor(0, 0, 0, 100)
 
   await waitForNetwork()
 
   const ts = await tsConnect().catch(async () => {
-    if(envVars.DEBUG_UI) await streamDeck.fillKeyColor(0, 100, 0, 0)
+    if (envVars.DEBUG_UI) await streamDeck.fillKeyColor(0, 100, 0, 0)
     throw ts
   })
-  if(envVars.DEBUG_UI) await streamDeck.fillKeyColor(0, 0, 100, 0)
+  if (envVars.DEBUG_UI) await streamDeck.fillKeyColor(0, 0, 100, 0)
 
   streamDeck.on('down', async (keyIndex: number) => {
     const cl = staticData.clientOnDeck[keyIndex]
@@ -39,9 +39,30 @@ const run = async () => {
     await ts.clientMove(cl, staticData.prevCid)
   })
 
+  ts.on("clientconnect", ({client}) => {
+    console.log(`client connect: ${client.nickname}`)
+    TsDrawClients(streamDeck, ts)
+  })
+  ts.on("clientdisconnect", ({client}) => {
+    console.log(`client disconnect: ${client?.nickname}`)
+    TsDrawClients(streamDeck, ts)
+  })
+  ts.on("clientmoved", ({client}) => {
+    console.log(`client moved: ${client.nickname}`)
+    TsDrawClients(streamDeck, ts)
+  })
+
   while (true) {
-    await TsDrawClients(streamDeck, ts)
-    await wait(500);
+    const clients = await TsDrawClients(streamDeck, ts)
+
+    if (clients.length === 0) {
+      console.log("no one online: sleep some time...")
+      await drawClock(streamDeck)
+      await wait(30 * 1000)
+    } else {
+      await wait(60 * 1000);
+    }
+
   }
 }
 
--- src/teamspeak/tsPrintClients.ts
+++ src/teamspeak/tsPrintClients.ts
@@ -1,11 +1,10 @@
 import { StreamDeck } from "@elgato-stream-deck/node";
-import { TeamSpeak } from "ts3-nodejs-library";
+import { TeamSpeak, TeamSpeakClient } from "ts3-nodejs-library";
 import { isMainUser } from "./tsHelper";
-import { drawClock, streamDeckPaintTs } from "../streamdeck";
-import { wait } from "../helper";
+import { streamDeckPaintTs } from "../streamdeck";
 import { staticData } from "../index";
 
-export const TsDrawClients = async (streamDeck: StreamDeck, ts: TeamSpeak) => {
+export const TsDrawClients = async (streamDeck: StreamDeck, ts: TeamSpeak): Promise<TeamSpeakClient[]> => {
   const clientsRaw = (await ts.clientList()).filter(c => c.type === 0)
 
   const mainUser = clientsRaw.find(isMainUser)
@@ -20,18 +19,10 @@ export const TsDrawClients = async (streamDeck: StreamDeck, ts: TeamSpeak) => {
     await streamDeckPaintTs(streamDeck, client, i, idleTime)
   }
 
-  if(clients.length !== 0){
+  if (clients.length !== 0) {
     for (let i = clients.length; i < streamDeck.NUM_KEYS; i++) {
       await streamDeck.clearKey(i)
     }
   }
-
-  if (clients.length === 0) {
-    console.log("no one online: sleep some time...")
-    await drawClock(streamDeck)
-    await wait(10 * 1000)
-  } else if (!clients.find(isMainUser)) {
-    console.log("mainUser offline: sleep some time...")
-    await wait(5 * 1000)
-  }
+  return clients
 }
